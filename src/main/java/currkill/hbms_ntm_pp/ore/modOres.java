b1e4e9318a72231c034460ac8d1ad11f3251b67c
Including-potash <3536028606@qq.com>
2026-07-18 17:50:34 +0800

    ore blocks. e.g. copper ore

--- src/main/java/currkill/hbms_ntm_pp/ore/modOres.java
+++ src/main/java/currkill/hbms_ntm_pp/ore/modOres.java
@@ -22,15 +22,47 @@ public class modOres {
     public static final RegistryObject<Block> TUNGSTEN_ORE =
             registerOre("tungsten_ore", () -> new Block(BlockBehaviour.Properties.of()
                     .strength(5.0F,10.0F)
-                    .sound(SoundType.STONE)));//钨矿石
+                    .sound(SoundType.STONE)));//钨矿石 tungsten ore
+
     public static final RegistryObject<Block> TITANIUM_ORE =
             registerOre("titanium_ore", () -> new Block(BlockBehaviour.Properties.of()
                     .strength(5.0F,10.0F)
-                    .sound(SoundType.STONE)));//钛矿石
+                    .sound(SoundType.STONE)));//钛矿石 titanium ore
+
+    public static final RegistryObject<Block> CLUSTER_IRON_ORE =
+            registerOre("cluster_iron_ore", () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(5.0F,10.0F)
+                    .sound(SoundType.STONE)));//晶体铁矿石 cluster iron ore
+
+    public static final RegistryObject<Block> CLUSTER_TITANIUM_ORE =
+            registerOre("cluster_titanium_ore", () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(5.0F,10.0F)
+                    .sound(SoundType.STONE)));//晶体钛矿石 cluster titanium ore
+
+    public static final RegistryObject<Block> CLUSTER_COPPER_ORE =
+            registerOre("cluster_copper_ore", () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(5.0F,10.0F)
+                    .sound(SoundType.STONE)));//晶体铜矿石 cluster copper ore
+
+    public static final RegistryObject<Block> CLUSTER_DEPTH_IRON_ORE =
+            registerOre("cluster_depth_iron_ore", () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(5.0F,10.0F)
+                    .sound(SoundType.STONE)));//深层晶体铁矿石 cluster depth iron ore
+
+    public static final RegistryObject<Block> CLUSTER_DEPTH_TITANIUM_ORE =
+            registerOre("cluster_depth_titanium_ore", () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(5.0F,10.0F)
+                    .sound(SoundType.STONE)));//深层晶体钛矿石 cluster depth titanium ore
+
+    public static final RegistryObject<Block> CLUSTER_DEPTH_COPPER_ORE =
+            registerOre("cluster_depth_copper_ore", () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(5.0F,10.0F)
+                    .sound(SoundType.STONE)));//深层晶体铜矿石(原版有吗？) cluster depth copper ore(Does NTM has this thing?)
+
     public static final RegistryObject<Block> LEAD_ORE =
             registerOre("lead_ore", () -> new Block(BlockBehaviour.Properties.of()
                     .strength(5.0F,10.0F)
-                    .sound(SoundType.STONE)));//铅矿石
+                    .sound(SoundType.STONE)));//铅矿石 lead ore
 
 
     private static <T extends Block> void registerOreBlockItems(String name, RegistryObject<T> block) {
